6a6975c25186f8cd2d10522bb37b84a5afe4da72
Mayara Felix <may.cabral@hotmail.com>
2018-10-30 22:30:59 -0400

    Validate type, amount and note when save button is pressed.

--- app/src/main/java/com/example/mayarafelix/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/mayarafelix/dailyshoppinglist/HomeActivity.java
@@ -1,14 +1,17 @@
 package com.example.mayarafelix.dailyshoppinglist;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FloatingActionButton fab;
@@ -24,12 +27,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         fab = findViewById(R.id.activity_home_fab);
 
-        fab.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        launchInputDialog();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchInputDialog();
+            }
+        });
     }
 
     private void launchInputDialog() {
@@ -37,8 +40,40 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
         View view = inflater.inflate(R.layout.input_data, null);
 
-        AlertDialog dialog = builder.create();
+        final AlertDialog dialog = builder.create();
         dialog.setView(view);
+
+        final EditText typeView = view.findViewById(R.id.input_data_type);
+        final EditText amountView = view.findViewById(R.id.input_data_amount);
+        final EditText noteView = view.findViewById(R.id.input_data_note);
+        Button buttonSave = view.findViewById(R.id.btn_save);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+
+                String type = typeView.getText().toString().trim();
+                String amount = amountView.getText().toString().trim();
+                String note = noteView.getText().toString().trim();
+
+                if (TextUtils.isEmpty(type)) {
+                    typeView.setError("Required Field");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(amount)) {
+                    amountView.setError("Required Field");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(note)) {
+                    noteView.setError("Required Field");
+                    return;
+                }
+            }
+        });
+
         dialog.show();
     }
 }
