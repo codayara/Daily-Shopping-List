d4e340fb536dc2024b3fc3b6df61895630481247
Mayara Felix <may.cabral@hotmail.com>
2018-10-30 22:07:38 -0400

    Launch Input Dialog when the user clicks on the fab.

--- app/src/main/java/com/example/mayarafelix/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/mayarafelix/dailyshoppinglist/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.mayarafelix.dailyshoppinglist;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,24 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Daily Shopping List");
+
+        fab = findViewById(R.id.activity_home_fab);
+
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        launchInputDialog();
+    }
+
+    private void launchInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+        View view = inflater.inflate(R.layout.input_data, null);
+
+        AlertDialog dialog = builder.create();
+        dialog.setView(view);
+        dialog.show();
     }
 }
